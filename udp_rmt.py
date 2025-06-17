5b7799e76e2009d66c98565417014d14aa2699c9
Ruan <xslackx@live.fr>
2025-06-17 15:40:05 -0300

    Create udp_listen method

--- udp_rmt.py
+++ udp_rmt.py
@@ -1,15 +1,18 @@
-import random
 import socket
+from time import sleep
 
 MAX_BYTES = 1024
 
-def udp_listen(address: tuple):
+def udp_listen(port: int):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        sock.bind(address)
-        print("Listening : ", sock.getsockname())
+        sock.bind(("", port))
+        is_bind = True
     except:
+        is_bind = False
         return False
-
-def udp_client(host: tuple):
-    pass
+    if is_bind:
+        while is_bind:
+            data, guest = sock.recvfrom(MAX_BYTES)
+            print("Receive data: ", data.decode('utf-8'))
+            sock.sendto("*ESP*".encode('ascii'), guest)
